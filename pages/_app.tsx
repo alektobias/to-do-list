08d426a136921ab780361a9e09196b48d337ff74
Alek Tobias <alektobias@gmail.com>
2021-11-18 12:05:25 -0300

    feat: styles added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../store";
+import "../styles/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import TodoList from "../components/ToDoList";
+import styles from "../styles/home-page.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <main className={styles.HomePage}>
       <TodoList />
-    </div>
+    </main>
   );
 };
 
